138445cf9280e94be1f8ccf15551bb5db72c1854
Mediafly, Inc <support@mediafly.com>
2011-08-23 15:20:47 -0500

    Add tests

---
+++ tests/calls/test-data.js
@@ -0,0 +1,399 @@
+var tokenResponse = {
+    "response": {
+        "duration": "00:00:00.0181713",
+        "status": "ok",
+        "token": {
+            "id": "c8d3f6a1ba204a51b33030071fbcef52",
+            "value": "WGNURzd0T2dtQ1BZUEZKOW0yK01RNlJMRlYvU0JmVDBzUk0yVFJRTkNpUjBvMS9MZkJPeFdOUDkydUN1ZjNDKzUxbFF3bzVDcFZqc1E5U2IrOVZ2Z0VVNndqTjJXdTZoNDZsUWpieEYyNVJwNTdMTm9vZk02VlRIRisxc0xNWkV4TjBXYktxSHp0ZkFmYkZ2Q3ovQTRtNUtvRTk4UjlhMmZ1ZGNsUWJiK1ZWdFpUd0I3cCtSU2JQaWlpSXpsYy9TWFpob2NtNnh5dkxZczRFNjk1S3FWajY0MGFzdm51N2VuWDdzWUV0Sm16cTN2U1lQME5XY2pod0VmUHJrQ1FQWDVuYWJXdEpPZ3kzOFhMVzlQQ3FYcm03bGF5RWRJRjF4a3ZFT1lraUcwYWFqekRjRTJJenVpZTB5UCtCaE5ZV0tmbW4wakhITG5RTUVIL1N5c1QvZ0w4LytiZnhtRkM0RkJVZCtvbUZPb1Y1eTJEMXRNbDU0UWtxQ2NGTnJzWDZKSllnalUyQkVoVUYvU0lMWkZzNEI4OUFadS93Y2hsbWR3enNuTnpmYlpHMlJyNVhJMjIvNDB4QStsellFcFZOc1pPbWRPang1OUV4ZmlqVWtJU1U1ODBmaWZoOXc2dDBlNmw1dlhreWU4dGRjWnR5d3czbkpLQVlTZEtySGkxYXhWVVhsSG1RODdVOU5hS2c4NTd5ZjZsb2V0RGVpSzZZZS9sbG1qT2ZQWnRpWnludTAwKzRJbXdaRGdkMmM1YXkyNEZhMjYzL0F2emtZUnMxbjU2eFRIVUJmZWdQSWoxV2J5MURrYzFFajRoNFBlUUxRV0hqdEl2MnFleVJSSW9RaXZhektRL3BZUUVIM3VDYmZKTC9XOVloZTREemZvOEpsSGZOTXdkMDlJR2hkUlo5NmhOVG5Dc056NFF5Mm1TM3ZYcEt1WHN1UDZFbktZUjEvTWxtL2dwYzdmYzg0R1ROQkNEOXJraTBxOERTb3RjQ1o5NSs4Ti9pVzYzeVlTR3E5WitIMkRNTHYrcnRnODB3TTRCQ2JyS2gwQTZFVUtBTzh4K2tHM3RmUEh6Q0VTZFNhc1dhbnNKUVArbWJ1WEtYUkNsbFpnZ1plR3ZIRWJFNGNCczdxdStTa1NXZTRQdWlIL0VrSG5ZaFRHZmdXQTExb216ZlBjWVhoc2h0a0FSb2NCZlBpL0p6a09KS0hTM2huUzc5R3dKWmZpUk43enFzVjMrcHJQMnJNVTh0Y1F4K3VpaVp5SjhaQTRuQkF1UmJ3"
+        }
+    }
+};
+
+var channelsResponse = {
+    "response": {
+        "channels": {
+            "channel": [
+            {
+                "name": "All (Mix)",
+                "slug": "__all__"
+            },
+            {
+                "name": "Business And Finance",
+                "slug": "3f4c67bed8a64b04a3c3388479161400"
+            },
+            {
+                "name": "Cars",
+                "slug": "c41dd4d0455b4507972fcf5e1e457f51"
+            },
+            {
+                "name": "Comedy",
+                "slug": "7e51c1ef4f5f463bb48c6c144a3ccc9b"
+            },
+            {
+                "name": "Cooking And Drinks",
+                "slug": "67ebe3e620f74f43aa50c9e0e933a219"
+            },
+            {
+                "name": "Education",
+                "slug": "5fa2a730124646859d08645a6d247640"
+            },
+            {
+                "name": "Entertainment News",
+                "slug": "077fdfea8bc348559b8b7711f64af2e4"
+            },
+            {
+                "name": "Fashion And Luxury",
+                "slug": "506b3da61efe4c3baf0bebeda34319c2"
+            },
+            {
+                "name": "Health And Fitness",
+                "slug": "311f9d788a05436ab75ad4c38f15854e"
+            },
+            {
+                "name": "History",
+                "slug": "00d1028d6b424bac9250a8a42334926f"
+            },
+            {
+                "name": "Kids, Family And Parenting",
+                "slug": "25a83c89ee6c45ca9fb2da835b98f189"
+            },
+            {
+                "name": "Learn A Language",
+                "slug": "d7c2002a8a49464d92b5958f2e73b618"
+            },
+            {
+                "name": "Music",
+                "slug": "78474c97d3f54d988118635382a0440e"
+            },
+            {
+                "name": "News And Politics",
+                "slug": "f6520aa4ffab42629247965ec56f543b"
+            },
+            {
+                "name": "Relationships",
+                "slug": "1abe18bbe79247a78ae75d158a744118"
+            },
+            {
+                "name": "Science And Nature",
+                "slug": "19d0c070ecb8443b906e75657f929ce8"
+            },
+            {
+                "name": "Sports",
+                "slug": "16a40d96cbac4b028a76f8ef37722f5d"
+            },
+            {
+                "name": "Technology",
+                "slug": "e5261d82d1dd436ca4efc871b9781121"
+            },
+            {
+                "name": "Travel",
+                "slug": "acc9635066454211984656ee1e368728"
+            },
+            {
+                "name": "Video Games",
+                "slug": "8142f8ce2eb446239d22d7e3d1907ee5"
+            }
+            ]
+        },
+        "duration": "00:00:00.1231435",
+        "status": "ok"
+    }
+};
+
+var channelResponse = {
+    "response": {
+        "channel": {
+            "episode": [
+            {
+                "description": "8-07-09 -- We\u0027re changing out tune on federal spending, Congress will get new jets, and how Twitter and Facebook went down for the count.<br \/><br \/><br \/><br \/>",
+                "slug": "The_Wall_Street_Journal_This_Morning_August_7th",
+                "title": "The Wall Street Journal This Morning August 7th",
+                "url": "http:\/\/www.mediafly.com\/Podcasts\/Episodes\/The_Wall_Street_Journal_This_Morning_August_7th\/Media\/1c83287fef4641219acf0beee861cc81\/pod-wsjtmit.mp3",
+                'imageUrl': "image.png",
+                'mediaType': "video",
+                'published': "2009-08-17T14:00:08",
+                'showSlug': "slug_for_show"
+            },
+            {
+                "description": "Many investors have abandoned buy-and-hold strategies in favor of opportunistic trading ... a lot of mature workers are lowering their job-hunting expectations ... coming soon to your fridge: draft beer in a box ... and we check out the weekend\u0027s new movies. Jim Chesko reports.<br \/><br \/><br \/><br \/><br \/><br \/><br \/>",
+                "slug": "WSJs_Your_Money_Matters_Aug_7_2009",
+                "title": "WSJ\u0027s Your Money Matters, Aug. 7, 2009",
+                "url": "http:\/\/www.mediafly.com\/Podcasts\/Episodes\/WSJs_Your_Money_Matters_Aug_7_2009\/Media\/1c83287fef4641219acf0beee861cc81\/pod-wsjymm.mp3",
+                'imageUrl': "image.png",
+                'mediaType': "video",
+                'published': "2009-08-17T14:00:08",
+                'showSlug': "slug_for_show"
+            }
+            ]
+        },
+        "duration": "00:00:00.1231435",
+        "status": "ok"
+    }
+};
+
+var searchResponse = {
+    "response": {
+        "duration": "00:00:00.0811298",
+        "searchResults": {
+            "show": [
+            {
+                "averageRating": "",
+                "description": "Outdoor Oklahoma TV Show",
+                "episode": {
+                    "averageRating": "",
+                    "bookmark": "0",
+                    "defaultChannelSlug": "16a40d96cbac4b028a76f8ef37722f5d",
+                    "defaultChannelTitle": "sports",
+                    "description": "Oklahoma\u0027s first lady goes crappie fishing.<br \/><br \/><br \/>",
+                    "favoriteType": "None",
+                    "imageUrl": "http:\/\/static.mediafly.com\/images\/feeds\/54e07f7ba86a4d85be5bccede487e7ee\/icon-300x300.jpg",
+                    "mediaType": "Video",
+                    "published": "2009-07-26T12:00:00",
+                    "showSlug": "Outdoor_Oklahoma",
+                    "slug": "First_Lady_Fishing_for_Crappie",
+                    "title": "First Lady Fishing for Crappie",
+                    "totalRatings": "",
+                    "url": "http:\/\/www.mediafly.com\/Podcasts\/Episodes\/First_Lady_Fishing_for_Crappie\/Media\/kimhenrycrappie1.mp4",
+                    "urlOriginal": "http:\/\/feedproxy.google.com\/~r\/OutdoorOklahoma\/~5\/0jJQ19dcEmM\/kimhenrycrappie1.mp4",
+                    "userRating": "",
+                    "versionHash": "l64Okq4LmUXe6gjhb+vXOiAi0Xk="
+                },
+                "favoriteType": "None",
+                "imageUrl": "http:\/\/static.mediafly.com\/images\/feeds\/54e07f7ba86a4d85be5bccede487e7ee\/icon-300x300.jpg",
+                "slug": "Outdoor_Oklahoma",
+                "title": "Outdoor Oklahoma",
+                "totalRatings": "",
+                "url": "http:\/\/www.mediafly.com\/Podcasts\/Feeds\/Outdoor_Oklahoma",
+                "urlOriginal": "http:\/\/www.wildlifedepartment.com",
+                "userRating": ""
+            },
+            {
+                "averageRating": "",
+                "description": "Biting (and sometimes humorous) commentary on political and current events in the United States and around the world.",
+                "episode": {
+                    "averageRating": "",
+                    "bookmark": "0",
+                    "defaultChannelSlug":
+                    "f6520aa4ffab42629247965ec56f543b",
+                    "defaultChannelTitle": "news & politics",
+                    "favoriteType": "None",
+                    "imageUrl": "http:\/\/static.mediafly.com\/images\/feeds\/fbea4631e8ad4d7bbe9fee1ecddcc84a\/icon-300x300.jpg",
+                    "mediaType": "Audio",
+                    "published": "2009-05-31T16:14:10",
+                    "showSlug": "Sam_Deidre_Show",
+                    "slug": "Rogues_Rascals_and_Renegades_Struggle_to_Retain_Control",
+                    "title": "Rogues, Rascals and Renegades Struggle to Retain Control",
+                    "totalRatings": "",
+                    "url": "http:\/\/www.mediafly.com\/Podcasts\/Episodes\/Rogues_Rascals_and_Renegades_Struggle_to_Retain_Control\/Media\/show.mp3",
+                    "urlOriginal": "http:\/\/www.samanddeidreshow.com\/audio\/060109\/show.mp3",
+                    "userRating": "",
+                    "versionHash": "tqy3\/ONhRkF1L9sJawgC42WxMVs="
+                },
+                "favoriteType": "None",
+                "imageUrl": "http:\/\/static.mediafly.com\/images\/feeds\/fbea4631e8ad4d7bbe9fee1ecddcc84a\/icon-300x300.jpg",
+                "slug": "Sam_Deidre_Show",
+                "title": "Sam & Deidre Show",
+                "totalRatings": "",
+                "url": "http:\/\/www.mediafly.com\/Podcasts\/Feeds\/Sam_Deidre_Show",
+                "urlOriginal": "http:\/\/www.samanddeidreshow.com",
+                "userRating": ""
+            }
+            ]
+        }
+    }
+};
+
+var showResponse = {
+    "response": {
+        "duration": "00:00:00.0148091",
+        "showResponse": {
+            "show": {
+                "averageRating": "",
+                "description": "Interviews with management experts who zero in on key business ideas and how to implement them in organizations.",
+                "favoriteType": "None",
+                "imageUrl": "http:\/\/static.mediafly.com\/images\/feeds\/0fbd19ee69cb4737acd65cb5cfc2bac4\/icon-300x300.jpg",
+                "slug": "Harvard_Business_Video_IdeaCast",
+                "title": "Harvard Business Video IdeaCast",
+                "totalRatings": "",
+                "url": "http:\/\/www.mediafly.com\/Podcasts\/Feeds\/Harvard_Business_Video_IdeaCast",
+                "urlOriginal": "http%3A%2F%2Fhbdvideoideacast.blip.tv",
+                "userRating": ""
+            },
+            "status": "ok"
+        },
+        "status": "ok"
+    }
+};
+
+var episodesForShowResponse = {
+    "response": {
+        "duration": "00:00:00.0944888",
+        "episodes": {
+            "episode": [
+            {
+                "averageRating": "",
+                "bookmark": "0",
+                "defaultChannelSlug": "a2220418cfc44f5ba6ff585a893e41bc",
+                "defaultChannelTitle": "business",
+                "description": "Sylvia Ann Hewlett of the Center for Work-Life Policy on why Gen Y and Boomer employees prefer non-financial incentives.",
+                "favoriteType": "None",
+                "imageUrl": "http:\/\/static.mediafly.com\/images\/feeds\/0fbd19ee69cb4737acd65cb5cfc2bac4\/icon-300x300.jpg",
+                "mediaType": "Video",
+                "published": "2009-08-04T00:56:47",
+                "showSlug": "Harvard_Business_Video_IdeaCast",
+                "slug": "What_Motivates_Gen_Y_and_Boomer_Talent",
+                "title": "What Motivates Gen Y and Boomer Talent",
+                "totalRatings": "",
+                "url": "http:\/\/www.mediafly.com\/Podcasts\/Episodes\/What_Motivates_Gen_Y_and_Boomer_Talent\/Media\/1c83287fef4641219acf0beee861cc81\/Hbvideoideacast-WhatMotivatesGenYAndBoomerTalent271.mp4",
+                "urlOriginal": "http:\/\/feeds.harvardbusiness.org\/~r\/harvardbusiness\/videoideacast\/~5\/AWPcb7J8AJU\/Hbvideoideacast-WhatMotivatesGenYAndBoomerTalent271.mp4",
+                "userRating": "",
+                "versionHash": "jRDRZIhtYzkxn44D7yRY7boDlfQ="
+            },
+            {
+                "averageRating": "",
+                "bookmark": "0",
+                "defaultChannelSlug": "a2220418cfc44f5ba6ff585a893e41bc",
+                "defaultChannelTitle": "business",
+                "description": "Adam Werbach, Global CEO of Saatchi and Saatchi S and former Sierra Club president, is the author of Strategy for Sustainability",
+                "favoriteType": "None",
+                "imageUrl": "http:\/\/static.mediafly.com\/images\/feeds\/0fbd19ee69cb4737acd65cb5cfc2bac4\/icon-300x300.jpg",
+                "mediaType": "Video",
+                "published": "2009-07-21T21:37:32",
+                "showSlug": "Harvard_Business_Video_IdeaCast",
+                "slug": "Build_a_Strategy_for_Sustainability",
+                "title": "Build a Strategy for Sustainability",
+                "totalRatings": "",
+                "url": "http:\/\/www.mediafly.com\/Podcasts\/Episodes\/Build_a_Strategy_for_Sustainability\/Media\/1c83287fef4641219acf0beee861cc81\/Hbvideoideacast-BuildAStrategyForSustainability602.mp4",
+                "urlOriginal": "http:\/\/feeds.harvardbusiness.org\/~r\/harvardbusiness\/videoideacast\/~5\/Z4_cOFErTUY\/Hbvideoideacast-BuildAStrategyForSustainability602.mp4",
+                "userRating": "",
+                "versionHash": "F1wDP2BxvsAgqqdxsaDMcg8+Ljk="
+            }
+            ]
+        }
+    }
+}
+
+var bindUserResponse = {
+    "response": {
+        "duration": "00:00:00.0784923",
+        "status": "ok"
+    }
+};
+
+var failedBindUserResponse = {
+    "response": {
+        "duration": "00:00:00.0097828",
+        "err": {
+            "code": "6",
+            "message": "No such username\/password"
+        },
+        "status": "fail"
+    }
+};
+
+var userAssociationCodeResponse = {
+    "response": {
+        "code": {
+            "value": "A7trHAQ5n"
+        },
+        "duration": "00:00:00.0058552",
+        "status": "ok"
+    }
+};
+
+var boundUserResponse = {
+    "response": {
+        "duration": "00:00:00.0082538",
+        "mfusers": {
+            "mfuser": [
+            {
+                "accountName": "rkolak",
+                "default": "true"
+            },
+            {
+                "accountName": "user",
+                "default": "false"
+            }
+            ]
+        },
+        "status": "ok"
+    }
+};
+
+var defaultSingleBoundUserResponse = {
+    "response": {
+        "duration": "00:00:00.0082538",
+        "mfusers": {
+            "mfuser": {
+                "accountName": "rkolak",
+                "default": "true"
+            }
+        },
+        "status": "ok"
+    }
+};
+
+var noBoundUsersResponse = {
+    "response": {
+        "duration": "00:00:00.0091589",
+        "mfusers": null,
+        "status": "ok"
+    }
+};
+
+var unbindUserResponse = {
+    "response": {
+        "duration": "00:00:00.0547530",
+        "status": "ok"
+    }
+};
+
+var bindUserAsDefaultResponse = {
+    "response": {
+        "duration": "00:00:00.0333841",
+        "status": "ok"
+    }
+};
+
+var successPollAssociationCodeResponse = {
+    "response": {
+        "associationCode": {
+            "code": "A7trHBT4X",
+            "status": "Linked"
+        },
+        "duration": "00:00:00.0076006",
+        "status": "ok"
+    }
+};
+
+var pollAssociationCodeResponse = {
+    "response": {
+        "associationCode": {
+            "code": "A7trHBT4X",
+            "status": "Pending"
+        },
+        "duration": "00:00:00.0076006",
+        "status": "ok"
+    }
+};
+
+var addFavoriteShowResponse = {
+    "response": {
+        "duration": "00:00:00.0318599",
+        "status": "ok"
+    }
+};
+
+var addFavoriteEpisodeResponse = {
+    "response": {
+        "duration": "00:00:00.0318599",
+        "status": "ok"
+    }
+};
+
+var addEpisodeRatingOkResponse = {
+    "response": {
+        "duration": "00:00:00.0723608",
+        "status": "ok"
+    }
+}
